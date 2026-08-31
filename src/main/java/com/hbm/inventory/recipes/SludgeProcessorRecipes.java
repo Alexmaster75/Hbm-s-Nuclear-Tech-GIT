651910e8b0daac42510d5df6c3ea3cad00a08f4f
purpl3xity <atmosphere176@gmail.com>
2026-08-31 14:15:31 +0100

    🫨 Sheam

--- src/main/java/com/hbm/inventory/recipes/SludgeProcessorRecipes.java
+++ src/main/java/com/hbm/inventory/recipes/SludgeProcessorRecipes.java
@@ -72,5 +72,13 @@ public class SludgeProcessorRecipes extends GenericRecipes<SludgeProcessorRecipe
 			.outputFluids(
 				new FluidStack(Fluids.WATER, 2000)
 			));
+
+		this.register((SludgeProcessorRecipe) new SludgeProcessorRecipe("sludge.sand_slop_processing")
+			.setup(50, 5_000)
+			.inputFluids(new FluidStack(Fluids.HEAVY_SAND_SLOP, 1000))
+			.outputItems(new ItemStack(ModItems.powder_zirconium, 2))
+			.outputFluids(new FluidStack(Fluids.MONAZITE_SLOP, 500), new FluidStack(Fluids.CHLORINE, 500))
+			.setIconToFirstIngredient()
+		);
 	}
 }
